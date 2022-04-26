d5d51b1f68ca2b7483665151b95d82c0e6d0eca7
Matt <mckaymatt@outlook.com>
2022-04-26 16:17:47 -0400

    First commit skeleton of back end

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  resources :rooms
+  # changed from get pages/home to root pages#home
+  root 'pages#home'
+  devise_for :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_scope :user do
+    get 'users', to: 'devise/sessions#new' 
+  end
+  get 'user/:id', to: 'users#show', as: 'user'
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
