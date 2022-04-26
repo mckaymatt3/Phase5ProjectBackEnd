23f6274ba032c2f24307ce5cc6b5e3ad56159e1d
Matt <mckaymatt@outlook.com>
2022-04-26 16:58:06 -0400

    Create room functionality and some styling

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,8 +2,15 @@ class RoomsController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @room = Room.new
     @rooms = Room.public_rooms
     @users = User.all_except(current_user)
   end
+
+  # create room below
+  def create
+    @room = Room.create(name: params['room']['name'])
+  end
+
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
+  
+  #create access to routes for rooms
   resources :rooms
+  
   # changed from get pages/home to root pages#home
   root 'pages#home'
+  
   devise_for :users
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   devise_scope :user do
     get 'users', to: 'devise/sessions#new' 
   end
+
   get 'user/:id', to: 'users#show', as: 'user'
 
   # Defines the root path route ("/")
