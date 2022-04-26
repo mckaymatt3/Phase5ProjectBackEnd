843aed1a4f6afd94efa5e42f7caecbf8bd749521
Matt <mckaymatt@outlook.com>
2022-04-26 17:18:09 -0400

    Some styling and create room and show working

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -4,13 +4,33 @@ class RoomsController < ApplicationController
   def index
     @room = Room.new
     @rooms = Room.public_rooms
+
     @users = User.all_except(current_user)
+        @room = Room.new
+    @rooms = Room.public_rooms
+    render 'index'
+  end
+
+  #to show the rooms
+  def show
+    @single_room = Room.find(params[:id])
+    
+    @room = Room.new
+    @rooms = Room.public_rooms
+
+    @message = Message.new
+    @messages = @single_room.messages.order(created_at: :asc)
+
+    @users = User.all_except(current_user)
+    render 'index'
   end
 
   # create room below
   def create
     @room = Room.create(name: params['room']['name'])
   end
+
+
 
 end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+          :recoverable, :rememberable, :validatable
   scope :all_except, ->(user) { where.not(id: user) } 
   after_create_commit { broadcast_append_to "users" }
+
+  has_many :messages
+
 end
